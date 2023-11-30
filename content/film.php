eb7d0e11ade9bde048f0c4b1b764c5bfbea0ab3b
fathaan <namanyaaalfathan2898.gmail.com>
2023-11-30 19:49:24 +0700

    database Film

---
+++ content/film.php
@@ -0,0 +1,46 @@
+<!-- ------------- HEADER ------------- -->
+<?php require_once("template/header.php"); ?>
+<!-- ---------------------------------- -->
+
+<?php //DATABASE CONNECT
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "db_film";
+
+$koneksinya = new mysqli($hostname, $username, $password, $database);
+
+$hasilData = $koneksinya->query("SELECT*FROM film");
+?>
+
+<!-- ______________ MULAI KONTEN ______________ -->
+    <div class="row">
+        <div class="col-xl-11">
+            <!-- ISI KONTEN -->
+            <table class="table">
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Judul</th>
+                            <th scope="col">Genre</th>
+                            <th scope="col">Durasi</th>
+                            <th scope="col">Cover</th>
+                            <th scope="col">Sutradara</th>  
+                        </tr>
+                    <?php $i = 1; foreach($hasilData as $data):?>
+                        <tr>
+                            <th scope="row"> <?=$i++?>. </th>
+                            <td><?= $data['judulFilm'] ?></td>
+                            <td><?= $data['genreFilm'] ?></td>
+                            <td><?= $data['durasiFilm'] ?></td>
+                            <td><img src="<?= $data['cover'] ?> " width="60"></td>
+                            <td><?= $data['sutradara'] ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+        </div>
+    </div>
+<!-- ______________ AKHIR KONTEN ______________ -->
+
+<!-- ------------- FOOTER ------------- -->
+<?php require_once("template/footer.php"); ?>
+<!-- ---------------------------------- -->
---
+++ content/index.php
@@ -0,0 +1,11 @@
+
+<?php require_once("template/header.php"); ?>
+
+<!-- MULAI KONTEN -->
+        <div class="row">
+            <div class="col-xl-10">
+
+            </div>
+        </div>
+<!-- END KONTEN -->
+<?php require_once("template/footer.php"); ?>
